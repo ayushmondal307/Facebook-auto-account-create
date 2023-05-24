35039a6a5f89237cbaf1170b83f6770f286fc859
Ayush Mondal <75127242+ayushmondal307@users.noreply.github.com>
2023-05-24 21:10:14 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -99,6 +99,10 @@ lastname = ["Alan", "Murat", "Azad", "Necati", "Aaron", "Aaron-James", "Aarron",
                  "Burhan", "Butali", "Butchi", "Byron", "Cabhan", "Cadan", "Cade", "Caden", "Cadon", "Cadyn", "Caedan",
                  "Caedyn", "Cael", "Caelan", "Caelen", "Caethan", "Cahl", "Cahlum", "Cai", "Caidan", "Melim"]
 
+day = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+month = [1,2,3,4,5,6,7,8,9,10,11,12]
+year =[1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003]
+
 password = ["pass123$%","elen23$51","word324%","length12#$%","face23$%&1","hru231%$#"] #replace with your required password here...
 
 driver = webdriver.Chrome()
@@ -132,17 +136,17 @@ driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[2]/div/div/di
 time.sleep(2)
 
 dropdown = Select(driver.find_element_by_id("day"))
-dropdown.select_by_value("4")
+dropdown.select_by_value(random.choice(day))
 
 time.sleep(1)
 
 dropdown = Select(driver.find_element_by_id("month"))
-dropdown.select_by_value("6")
+dropdown.select_by_value(random.choice(month))
 
 time.sleep(1)
 
 dropdown = Select(driver.find_element_by_id("year"))
-dropdown.select_by_value("1985")
+dropdown.select_by_value(random.choice(year))
 
 time.sleep(2)
 
